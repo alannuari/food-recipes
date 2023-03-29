1dad0396c8bcb01e9911a77c089b24facb41ab8b
alannuari <alannuari372@gmail.com>
2023-03-29 21:08:45 +0700

    tambah bottom tab dan screen my recipes

--- src/navigations/ButtomTab.js
+++ src/navigations/ButtomTab.js
@@ -7,6 +7,7 @@ import FavoriteScreen from '../screens/FavoriteScreen';
 import {useSelector} from 'react-redux';
 import AddRecipeScreen from '../screens/AddRecipeScreen';
 import {color} from '@rneui/base';
+import MyRecipesScreen from '../screens/MyRecipesScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,15 +19,14 @@ const ButtomTab = () => {
   return (
     <Tab.Navigator
       screenOptions={({route}) => ({
-        tabBarShowLabel: false,
+        tabBarLabelStyle: {
+          marginBottom: 5,
+        },
         tabBarStyle: {
           borderTopEndRadius: 15,
           borderTopLeftRadius: 15,
-          height: 60,
-        },
-        tabBarIconStyle: {
-          fontSize: 40,
-          backgroundColor: 'red',
+          height: 65,
+          paddingVertical: 10,
         },
         tabBarIcon: ({focused, color, size}) => {
           size = 30;
@@ -41,16 +41,20 @@ const ButtomTab = () => {
             iconName = focused ? 'heart' : 'heart-outline';
           } else if (route.name === 'Add Recipe') {
             iconName = focused ? 'add-circle' : 'add-circle-outline';
+          } else if (route.name === 'My Recipes') {
+            iconName = focused ? 'book' : 'book-outline';
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
+        tabBarActiveTintColor: 'gray',
       })}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{title: 'Category List'}}
       />
+      <Tab.Screen name="My Recipes" component={MyRecipesScreen} />
       <Tab.Screen name="Add Recipe" component={AddRecipeScreen} />
       <Tab.Screen
         name="Favorites"
