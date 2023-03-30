e0f5700e6c7f1e568ae404b5f3b4a8a4370660c5
alannuari <alannuari372@gmail.com>
2023-03-31 05:31:40 +0700

    add post reducer to root reducer

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,12 +1,14 @@
 import {combineReducers} from 'redux';
 import CategoryReducer from './CategoryReducer';
 import FavoriteReducer from './FavoriteReducer';
+import PostReducer from './PostReducer';
 import RecipeReducer from './RecipeReducer';
 
 const rootReducer = combineReducers({
   category: CategoryReducer,
   recipe: RecipeReducer,
   favorite: FavoriteReducer,
+  post: PostReducer,
 });
 
 export default rootReducer;
