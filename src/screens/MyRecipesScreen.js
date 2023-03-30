3ef958c83961e87905a3aec6d9d10ab2850fb41d
alannuari <alannuari372@gmail.com>
2023-03-31 05:05:54 +0700

    add data to my recipe screen

--- src/screens/MyRecipesScreen.js
+++ src/screens/MyRecipesScreen.js
@@ -1,11 +1,64 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-const MyRecipesScreen = () => {
+import React, {useContext, useEffect, useState} from 'react';
+import {View, FlatList, StyleSheet} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import {AuthContext} from '../navigations/AuthProvider';
+import RecipeItem from '../components/RecipeItem';
+import SkeletonItem from '../components/SkeletonItem';
+
+const MyRecipesScreen = ({navigation}) => {
+  const {user} = useContext(AuthContext);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let arr = [];
+    try {
+      setLoading(true);
+      firestore()
+        .collection('Posts')
+        .where('userId', '==', user.uid)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            arr.push({...doc.data(), id: doc.id});
+          });
+          setData(arr);
+        });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   return (
-    <View>
-      <Text>My Recipe</Text>
+    <View style={styles.container}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        data={loading ? Array(8).fill() : data}
+        renderItem={({item}) => {
+          return loading ? (
+            <SkeletonItem />
+          ) : (
+            <RecipeItem
+              id={item.id}
+              title={item.title}
+              imageUrl={item.imageUrl}
+              navigation={navigation}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 5,
+  },
+});
 
 export default MyRecipesScreen;
