dcc0418f52ec88fc2fc2f43bf73fd1419a01a443
alannuari <alannuari372@gmail.com>
2023-03-31 05:04:46 +0700

    fetch data favorite when home page is rendering

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,21 @@
-import React, {useLayoutEffect} from 'react';
+import React, {useLayoutEffect, useContext} from 'react';
 import {View, FlatList, StyleSheet} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import CategoryItem from '../components/CategoryItem';
 import SkeletonItem from '../components/SkeletonItem';
+import {AuthContext} from '../navigations/AuthProvider';
 import {fetchCategory} from '../store/action/category';
+import {fetchFavorite} from '../store/action/favorite';
 
 const HomeScreen = ({navigation}) => {
+  const {user} = useContext(AuthContext);
   const {data, loading} = useSelector(state => state.category);
   const dispatch = useDispatch();
 
   useLayoutEffect(() => {
     dispatch(fetchCategory());
-  }, [dispatch]);
+    dispatch(fetchFavorite(user.uid));
+  }, [dispatch, user.uid]);
 
   return (
     <View style={styles.container}>
