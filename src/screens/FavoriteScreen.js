c517e0f014110f2ec60664109b81593bc9350122
alannuari <alannuari372@gmail.com>
2023-03-31 05:03:25 +0700

    add param user id to fetch data favorite

--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,18 +1,20 @@
-import React, {useLayoutEffect} from 'react';
+import React, {useLayoutEffect, useContext} from 'react';
 import {View, FlatList, StyleSheet} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import FavoriteItem from '../components/FavoriteItem';
 import SkeletonFavorite from '../components/SkeletonFavorite';
+import {AuthContext} from '../navigations/AuthProvider';
 import {fetchFavorite} from '../store/action/favorite';
 import NotFoundScreen from './NotFoundScreen';
 
 const FavoriteScreen = ({navigation}) => {
+  const {user} = useContext(AuthContext);
   const {data, loading} = useSelector(item => item.favorite);
   const dispatch = useDispatch();
 
   useLayoutEffect(() => {
-    dispatch(fetchFavorite());
-  }, [dispatch]);
+    dispatch(fetchFavorite(user.uid));
+  }, [dispatch, user.uid]);
 
   return (
     <View style={styles.container}>
