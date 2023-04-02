911b92f64707b120fd0ba126b59b25f88ec41264
alannuari <alannuari372@gmail.com>
2023-04-02 13:43:18 +0700

    update styling splash screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,19 +1,26 @@
 import React, {useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import Love from '../assets/images/love.svg';
+import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
+import Cook from '../assets/images/cook.svg';
 
 const SplashScreen = ({navigation}) => {
   useEffect(() => {
     setTimeout(() => {
       navigation.replace('Login');
-    }, 3000);
+    }, 4000);
   }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.image}>
-        <Love width={'100%'} height={'100%'} />
+        <Cook width={'100%'} height={'100%'} />
       </View>
-      <Text style={styles.text}>Welcome to Food Recipes</Text>
+      <View style={styles.title}>
+        <Text style={styles.text}>Welcome to Food Recipes</Text>
+        <Text style={styles.subText}>
+          Explore recipes and enjoy your cooking!
+        </Text>
+        <ActivityIndicator size="large" color="gray" />
+      </View>
     </View>
   );
 };
@@ -27,13 +34,22 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
   },
+  title: {
+    height: 300,
+  },
   text: {
+    marginTop: 30,
     textAlign: 'center',
     alignSelf: 'center',
-    height: 300,
-    fontSize: 30,
+    fontSize: 28,
     fontWeight: 'bold',
     width: 230,
+  },
+  subText: {
+    marginTop: 20,
+    marginBottom: 50,
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
 
