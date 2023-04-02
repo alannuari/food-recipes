1f347ce6ea3d55fecca03f74706fb2b026ebd5f9
alannuari <alannuari372@gmail.com>
2023-04-02 13:17:59 +0700

    update styling profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,6 +1,5 @@
 import React, {useContext, useLayoutEffect} from 'react';
 import {View, Text, ScrollView, Image, StyleSheet, Alert} from 'react-native';
-import ButtonComponent from '../components/ButtonComponent';
 import {AuthContext} from '../navigations/AuthProvider';
 import HeaderButtonIcon from '../components/HeaderButtonIcon';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
@@ -36,21 +35,28 @@ const ProfileScreen = ({navigation}) => {
 
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={{uri: 'https://www.kindpng.com/picc/b/136/1369892.png'}}
-          style={styles.image}
-          resizeMode="center"
-        />
+      <View style={styles.info}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{uri: 'https://www.kindpng.com/picc/b/136/1369892.png'}}
+            style={styles.image}
+            resizeMode="center"
+          />
+        </View>
+        <View style={styles.identity}>
+          <Text style={styles.name}>Dummy Name</Text>
+          <Text style={styles.bio}>No Bio</Text>
+          <Text style={styles.email}>email : {user.email}</Text>
+        </View>
       </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.name}>Alan Nuari</Text>
-        <Text style={styles.work}>Frontend Developer</Text>
+      <Text style={styles.text}>Information</Text>
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Total Favorite</Text>
+        <Text style={styles.cardContent}>20</Text>
       </View>
-      <Text style={styles.info}>Information</Text>
-      <View style={styles.recentItem}>
-        <Text>~ email : {user.email}</Text>
-        <Text>~ country : Indonesia</Text>
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Total Recipe Post</Text>
+        <Text style={styles.cardContent}>10</Text>
       </View>
     </ScrollView>
   );
@@ -59,8 +65,13 @@ const ProfileScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginVertical: 25,
-    marginHorizontal: 5,
+    margin: 10,
+  },
+  info: {
+    backgroundColor: '#e1e1e1',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    borderRadius: 15,
   },
   imageContainer: {
     alignItems: 'center',
@@ -71,28 +82,48 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     borderRadius: 50,
   },
-  infoContainer: {
+  identity: {
     marginVertical: 20,
     alignItems: 'center',
   },
   name: {
     fontWeight: 'bold',
-    fontSize: 30,
+    fontSize: 24,
   },
-  work: {
+  bio: {
     color: '#AEB5BC',
-    fontSize: 18,
+    marginVertical: 5,
   },
-  info: {
+  email: {
+    marginVertical: 5,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 30,
+    marginBottom: 10,
+    marginHorizontal: 10,
+  },
+  card: {
+    backgroundColor: 'white',
+    height: 100,
+    marginBottom: 10,
+    borderRadius: 15,
+    paddingVertical: 15,
+  },
+  cardTitle: {
+    borderBottomColor: '#e1e1e1',
+    borderBottomWidth: 1,
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cardContent: {
+    paddingTop: 10,
+    textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 18,
-    marginVertical: 5,
-    marginHorizontal: 35,
-  },
-  recentItem: {
-    backgroundColor: 'white',
-    padding: 20,
-    marginHorizontal: 35,
   },
 });
 
